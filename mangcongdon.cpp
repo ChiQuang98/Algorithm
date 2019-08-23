c52301577d54f75fdb4fcab17ac0671fb97cda85
ChiQuang98 <35691187+ChiQuang98@users.noreply.github.com>
2019-08-23 22:03:11 +0700

    Update mangcongdon.cpp

--- mangcongdon.cpp
+++ mangcongdon.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+int s[7]={0};
 int main()
 {
 	//freopen("input.txt","r",stdin);
 	int arr[] = {8,1,5,7,2,4};
-	int s[7]={0};
-	for(int i=1;i<6;i++){
-		s[i] = arr[i] + s[i-1];
+	
+	s[1] = arr[0];
+	for(int i=2;i<=6;i++){
+		s[i] = arr[i-1] + s[i-1];
 	}
-	cout<<
+	cout<<s[5]-s[1];
 	return 0;
 }
-
